9bafa193d727bf9d604e130eb613bf22502c30d8
dISCOeRG0sUM <jendrik@freezeframes.de>
2019-11-21 21:28:55 +0100

    del

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-// prettier.config.js or .prettierrc.js
-module.exports = {
-  trailingComma: "all",
-  tabWidth: 2,
-  semi: true,
-  jsxSingleQuote: true,
-  arrowParens: "always",
-  // printWidth: 80,
-};
